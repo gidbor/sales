31690cc28af44c7db340bfa36708e15fedae550d
Francisco Becerra Rodriguez <pachonetworking@gmail.com>
2022-06-16 19:18:30 -0500

    Formulario reactivo crea usuarios operativo

--- src/app/components/users/createuser/createuser.component.ts
+++ src/app/components/users/createuser/createuser.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-createuser',
   templateUrl: './createuser.component.html',
-  styleUrls: ['./createuser.component.sass']
+  styleUrls: ['./createuser.component.sass'],
 })
 export class CreateuserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public createUserForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.createUserForm = this.formBuilder.group({
+      nameUser: '',
+      lastNameUser: '',
+      ageUser: '',
+    });
   }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.createUserForm.value);
+  }
+
+  //faltan validadores
+  //falta crear servicio
+  //fatta conectar con firebase
 
 }
--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { CreateuserComponent } from './createuser/createuser.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CreateuserComponent } from './createuser/createuser.component';
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UsersModule { }
